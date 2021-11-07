c12976e99f22ba26aa73ddfe07652a3cc822f477
michaelgorziza104@gmail.com <michaelgorziza104@gmail.com>
2021-11-07 18:47:55 -0300

    Layers, Pagination

--- backend/src/main/java/main/services/SaleService.java
+++ backend/src/main/java/main/services/SaleService.java
@@ -3,18 +3,24 @@ package main.services;
 import main.dtos.SaleDTO;
 import main.entities.Sale;
 import main.repositories.SaleRepository;
+import main.repositories.SellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
 public class SaleService {
     @Autowired
     private SaleRepository repository;
+    @Autowired
+    private SellerRepository sellerRepository;
 
+    @Transactional(readOnly = true)
     public Page<SaleDTO> findAll(Pageable pageable) {
+        sellerRepository.findAll();
         Page<Sale> list = repository.findAll(pageable);
         return list.map(x -> new SaleDTO(x));
     }
