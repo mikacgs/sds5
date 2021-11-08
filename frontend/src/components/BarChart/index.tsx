836dfdaaaa87bf04b5a1296744106c1e242191dd
michaelgorziza104@gmail.com <michaelgorziza104@gmail.com>
2021-11-07 22:07:49 -0300

    Config deploy

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -61,17 +61,17 @@ function BarChart() {
       },
    };
 
-   const mockData = {
-      labels: {
-         categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-      },
-      series: [
-         {
-            name: "% Sucesso",
-            data: [43.6, 67.1, 67.7, 45.6, 71.1]
-         }
-      ]
-   };
+   // const mockData = {
+   //    labels: {
+   //       categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+   //    },
+   //    series: [
+   //       {
+   //          name: "% Sucesso",
+   //          data: [43.6, 67.1, 67.7, 45.6, 71.1]
+   //       }
+   //    ]
+   // };
 
    return (
       <Chart
