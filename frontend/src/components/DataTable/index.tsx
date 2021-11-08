20173528c4a73eeae5353bd55a5e19216f8700d9
michaelgorziza104@gmail.com <michaelgorziza104@gmail.com>
2021-11-07 21:55:09 -0300

    Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,10 +1,17 @@
 import axios from "axios";
+import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { format } from "util";
 import { BASE_URL } from "utils/requests";
 
 function DataTable() {
+
+   const [activePage, setActivePage] = useState(0);
+
+   const changePage = (next: number) => {
+      setActivePage(next);
+   }
 
    const [page, setPage] = useState<SalePage>({
       first: true,
@@ -14,42 +21,45 @@ function DataTable() {
       totalPages: 0,
    })
    useEffect(() => {
-      axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
+      axios.get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`)
          .then(response => {
             setPage(response.data);
             // console.log(page);
 
          })
-   }, [])
+   }, [activePage])
 
    return (
-      <div className="table-responsive">
-         <table className="table table-striped table-sm">
-            <thead>
-               <tr>
-                  <th>Data</th>
-                  <th>Vendedor</th>
-                  <th>Clientes visitados</th>
-                  <th>Negócios fechados</th>
-                  <th>Valor</th>
-               </tr>
-            </thead>
-            <tbody>
-               {console.log(page)}
-               {
-                  page.content?.map(x => {
-                    return (<tr key={x.id}>
-                        <td>{format(x.date)}</td>
-                        <td>{x.seller.name}</td>
-                        <td>{x.visited}</td>
-                        <td>{x.deals}</td>
-                        <td>{x.amount.toFixed(2)}</td>
-                     </tr>)
-                  })
-               }
-            </tbody>
-         </table>
-      </div>
+      <>
+         <Pagination page={page} onPageChanged={changePage}></Pagination>
+         <div className="table-responsive">
+            <table className="table table-striped table-sm">
+               <thead>
+                  <tr>
+                     <th>Data</th>
+                     <th>Vendedor</th>
+                     <th>Clientes visitados</th>
+                     <th>Negócios fechados</th>
+                     <th>Valor</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  {console.log(page)}
+                  {
+                     page.content?.map(x => {
+                        return (<tr key={x.id}>
+                           <td>{format(x.date)}</td>
+                           <td>{x.seller.name}</td>
+                           <td>{x.visited}</td>
+                           <td>{x.deals}</td>
+                           <td>{x.amount.toFixed(2)}</td>
+                        </tr>)
+                     })
+                  }
+               </tbody>
+            </table>
+         </div>
+      </>
    );
 }
 
